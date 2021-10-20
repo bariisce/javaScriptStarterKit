8f1d4fc374de0e27e88684977823a854e1682de2
BARIŞ CEYLAN <202803012@ogr.cbu.edu.tr>
2021-10-21 01:03:21 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,14 @@
+console.log("Merhaba Kodlama.io")
+let dolarDun=9.20
+let dolarBugun = 9.30
+console.log(dolarDun)
+const euroDun=11.2
+console.log(euroDun)
+
+let konutKredileri = ["Konut kredisi","Emlak konut kredisi","taşıt kredisi"]
+console.log("<ul>")
+for (let index = 0; index < konutKredileri.length; index++) 
+{
+    console.log("<li>"+konutKredileri[index]+"</li>")
+}
+console.log("</ul>")
