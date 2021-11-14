276d9aef674b7c6361599b324d5c991e1f02c8df
BARIŞ CEYLAN <202803012@ogr.cbu.edu.tr>
2021-11-14 16:14:09 +0300

    😎😎

--- src/oop/odev1.4.js
+++ src/oop/odev1.4.js
@@ -1,2 +1,17 @@
-let sayi
-let i
+for (let i = 2; i < 1000; i++) 
+{ 
+    let asalSayi = 0;
+
+    for (let j =2; j < i; j++) 
+    {
+        if(i% j == 0)
+        {
+            asalSayi++;    
+        }
+    }
+
+    if (asalSayi == 0) 
+    {
+        console.log(i+ " asal sayıdır.") 
+    }
+}
